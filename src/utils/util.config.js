9acf31d9ba04d76bb397d7785349d47360fea212
Dineshkuna <146323388+Dineshkuna@users.noreply.github.com>
2026-01-08 18:18:29 +0530

    Change BASEURL to production server

--- src/utils/util.config.js
+++ src/utils/util.config.js
@@ -1,5 +1,7 @@
 
-export const BASEURL = "http://localhost:8080"
+export const BASEURL = "https://sever-2c56.onrender.com"
+
+// export const BASEURL = "http://localhost:8080"
 
 
 export const token = localStorage.getItem("token")
